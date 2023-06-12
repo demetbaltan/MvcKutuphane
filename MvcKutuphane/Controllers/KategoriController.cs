ddacaa1e265f83bd4e037cff9ba3edf0f4e8b49a
term1827 <term1827@BGANB034>
2023-06-12 09:09:45 +0300

    Proje dosyası ekle.

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Enitity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); 
+        public ActionResult Index()  //ındex sağ  tık ekle view, oluşacak olan sayfada yapılacak işlemlerin arayüzü view,işlemler burası
+        
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges(); //değişikliklerikaydet
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+        public ActionResult KategoriGetir(int id) 
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD = p.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ MvcKutuphane/Controllers/PersonelController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Enitity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLPERSONEL.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(TBLPERSONEL p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("PersonelEkle"); //Şartı sağlamadıysa bu viewi döndür.
+            }
+            db.TBLPERSONEL.Add(p);
+            db.SaveChanges(); //değişikliklerikaydet 
+            return View();
+        }
+        public ActionResult PersonelSil(int id)
+        {
+            var person = db.TBLPERSONEL.Find(id);
+            db.TBLPERSONEL.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            var prs = db.TBLPERSONEL.Find(id);
+            return View("PersonelGetir", prs);
+         }
+        public ActionResult PersonelGuncelle(TBLPERSONEL p)
+        {
+            var prs = db.TBLPERSONEL.Find(p.ID);
+            prs.PERSONEL = p.PERSONEL;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
